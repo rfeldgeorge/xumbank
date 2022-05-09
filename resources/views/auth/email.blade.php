573bd6153c0029328180fd45e997fe04167b464d
rfeldgeorge <rosenfeldgeorge190@gmail.com>
2022-05-10 00:02:00 +0100

    'xumbank'

--- resources/views/auth/email.blade.php
+++ resources/views/auth/email.blade.php
@@ -69,16 +69,16 @@
                 </a>
               </div>
               <!-- /Logo -->
-              <h4 class="mb-2">Online Banking</h4>
-              <p class="mb-4">Please sign-in to your account and start your online banking adventure</p>
+              <h4 class="mb-2">Welcome to Sneat Bank! 👋</h4>
+              <p class="mb-4">Please sign-in to your account and start the adventure</p>
 
               <form  class="mb-3" method="POST" action="{{ route('login') }}">
                     @csrf
                 <div class="mb-3">
-                  <label for="email" class="form-label">Account Email</label>
+                  <label for="email" class="form-label">Email</label>
                   <input
                     type="email"
-                    class="form-control @error('email') is-invalid @enderror" placeholder="Enter Account Email"
+                    class="form-control @error('email') is-invalid @enderror" placeholder="Enter Email"
                     name="email" value="{{ old('email') }}" 
                     id="email"
                     required autocomplete="email" autofocus>
@@ -88,37 +88,9 @@
                         </span>
                     @enderror
                 </div>
-                <div class="mb-3 form-password-toggle">
-                  {{-- <div class="d-flex justify-content-between">
-                    <label class="form-label" for="password">Password</label>
-                    <a href="{{ route('password.request') }}">
-                      <small>Forgot Password?</small>
-                    </a>
-                  </div> --}}
-                  <div class="input-group input-group-merge">
-                    <input
-                      type="password"
-                      id="password"
-                      class="form-control @error('password') is-invalid @enderror"
-                      name="password"
-                      placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                      name="password" required autocomplete="current-password">
-                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-                    @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                  </div>
-                </div>
+               
                 <div class="mb-3">
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                    <label class="form-check-label" for="remember-me"> Remember Me </label>
-                  </div>
-                </div>
-                <div class="mb-3">
-                  <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+                  <button class="btn btn-primary d-grid w-100" type="submit"> {{ __('Send Password Reset Link') }}</button>
                 </div>
               </form>
             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,83 +1,76 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<html
+  lang="en"
+  class="light-style customizer-hide"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="backend/assets/"
+  data-template="vertical-menu-template-free"
+>
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+    />
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Login Basic - Pages | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="description" content="" />
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="backend/assets/img/favicon/favicon.ico" />
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+      rel="stylesheet"
+    />
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="backend/assets/vendor/fonts/boxicons.css" />
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="backend/assets/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="backend/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="backend/assets/css/demo.css" />
 
-                    </ul>
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="backend/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="backend/assets/vendor/css/pages/page-auth.css" />
+    <!-- Helpers -->
+    <script src="backend/assets/vendor/js/helpers.js"></script>
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="backend/assets/js/config.js"></script>
+  </head>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
+  <body>
             @yield('content')
-        </main>
-    </div>
-</body>
+
+        <!-- Core JS -->
+        <!-- build:js assets/vendor/js/core.js -->
+        <script src="backend/assets/vendor/libs/jquery/jquery.js"></script>
+        <script src="backend/assets/vendor/libs/popper/popper.js"></script>
+        <script src="backend/assets/vendor/js/bootstrap.js"></script>
+        <script src="backend/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    
+        <script src="backend/assets/vendor/js/menu.js"></script>
+        <!-- endbuild -->
+    
+        <!-- Vendors JS -->
+    
+        <!-- Main JS -->
+        <script src="backend/assets/js/main.js"></script>
+    
+        <!-- Page JS -->
+    
+        <!-- Place this tag in your head or just before your close body tag. -->
+        <script async defer src="https://buttons.github.io/buttons.js"></script>
+    </body>
 </html>
